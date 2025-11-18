ca8709f98f4b26430443181068ee65ce00936888
humeng24 <914482467@qq.com>
2025-11-18 16:20:38 +0800

    Add files via upload

---
+++ module.py
@@ -0,0 +1,16 @@
+import os
+import re
+import time
+import torch
+import torch.utils.data
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+from torchvision import datasets, transforms
+from torch.autograd import Variable
+import numpy as np
+import pandas as pd
+import random
+
+
+__all__ = ['os', 're', 'time', 'torch', 'nn', 'F', 'optim', 'datasets', 'transforms', 'Variable', 'np', 'pd', 'random']
